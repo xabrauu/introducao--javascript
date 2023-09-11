f0593b18edd309ed866664be096aa1a1516f51b6
xabrauu <130366440+xabrauu@users.noreply.github.com>
2023-09-11 08:52:25 -0300

    Add files via upload

---
+++ java/introducao-javascript/js/principal.js
@@ -0,0 +1,13 @@
+//console.log("O arquivo separado esta funcionando!")
+
+//--------------------Daqui para baixo começa o codigo
+
+//alteração do titulo
+//console.log(document);
+//cria a variavel titulo e armazena o conteudo da class
+let titulo = document.querySelector(".titulo")
+//console.log(titulo);
+//atribuição de um novo valor na variavel titulo
+titulo.textContent = "Fake Natty Nutrição!";
+
+//alteração da tabela
